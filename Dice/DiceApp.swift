2ffce22f3323d3f65c76cb300d11ca8d6cc4f4be
ryomen666 <113868184+ryomen666@users.noreply.github.com>
2023-10-04 20:39:15 +0900

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by 森江遼 on 2023/10/04.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
